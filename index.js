015438b9ace628904020fa1257ef5c6ec95aca94
tongvietdung <vietdung.tong1998@gmail.com>
2021-03-24 00:08:00 +0100

    Changed contact. Add load effect
    Changed instagram contact to github.
    Added fade in effect when page load

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ $(document).ready(function () {
     var raisinBlack = "#252627ff";
     var outrageousOrange = "#fe6847ff";
 
+    /* When scroll, trigger the effect */
     window.onscroll = function() {
         scrollFunction()
     };
 
+    /* Effect */
     function scrollFunction() {
+        /* If mouse is scrolled (which is the verticle position of BODY or the ROOT element is > 50px) */
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            /* Find the navigation box and scale it down and move to the bottom of the screen */
             document.getElementById('navigation').style.transform = "scale(0.8) translateY(30vh)";
 
+            /* Find all items in navigation box and change its text color and background */
             for (let item of document.getElementsByClassName('navigation-item')) {
                 item.style.color = raisinBlack;
                 item.style.backgroundColor = "transparent";
             }
         } else {
+            /* Reset the state when scroll back to top */
             document.getElementById('navigation').style.transform = "scale(1)";
             for (let item of document.getElementsByClassName('navigation-item')) {
                 item.style.color = outrageousOrange;
